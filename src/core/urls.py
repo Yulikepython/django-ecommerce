ad6056ecc0619b79cbff1ba9c9296243ee3e7742
Yu <admin@itc.tokyo>
2021-03-15 00:04:33 +0900

    made order-summary and so on. upto 1:24:34

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
-from .views import HomeView, ProductView, add_to_cart, remove_from_cart
+from .views import (HomeView, 
+                    ProductView, 
+                    add_to_cart, 
+                    remove_from_cart,
+                    OrderSummaryView,
+                    )
 
 app_name="core"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("product/<slug:slug>/", ProductView.as_view(), name="product"),
+    path("order-summary/", OrderSummaryView.as_view(), name="order-summary"),
     path('add-to-cart/<slug:slug>/', add_to_cart, name="add-to-cart"),
     path('remove-from-cart/<slug:slug>/', remove_from_cart, name="remove-from-cart"),
 ]
